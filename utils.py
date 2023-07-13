42986e11fc4e7813df83638ed0eabe7accebe593
Eugene Malashenko <66912450+Eugeme@users.noreply.github.com>
2023-07-13 23:44:12 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from re import sub
+from datetime import datetime
 
 
 def camel_case(s):
     """rewrite string in camelCase format"""
-    s = sub(r"[_-]+", " ", s).title().replace(" ", "")
+    s = sub(r'[_-]+', ' ', s).title().replace(' ', '')
     return ''.join([s[0].lower(), s[1:]])
+
+
+def write_parquet(df):
+    """write df to parquet file"""
+    now = datetime.now()
+    dt_string = now.strftime('%d%m%Y%H%M%S')
+    df.write.parquet(f'output/{dt_string}.parquet')
